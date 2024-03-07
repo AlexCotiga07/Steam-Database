da96fa6e2dfd87be01c818dbdc333552bb0c2658
Alex Cotiga <20006@burnside.school.nz>
2024-03-08 09:50:57 +1300

    adding genres and genre bridging table

--- parsedata.py
+++ parsedata.py
@@ -2,15 +2,10 @@ import csv
 import sqlite3
 
 
-def cur_setup():
-    """connect to database"""
-    conn = sqlite3.connect("steam.db")
-    cursor = conn.cursor
-    return cursor
-
-
 def search_game(game):
-    """set up stuff for Game table"""
+    """set up stuff for Game table, put them into the table"""
+    conn = sqlite3.connect("steam.db")
+    cursor = conn.cursor()
     id = game[0]
     name = game[1]
     date = game[2].replace("/", "-")
@@ -34,19 +29,29 @@ def search_game(game):
     averagept = game[14]
     medianpt = game[15]
     price = "%.2f" % (float(game[17])*2.08)
-    return id, name, date, windows, mac, linux, minage, achievements, negatives, positives, averagept, medianpt, price
+    cursor.execute("INSERT INTO Game VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, name, date, windows, mac, linux, minage, achievements, negatives, positives, averagept, medianpt, price,))
+    conn.commit()
 
 
 def search_genres(game):
+    """Check if there are any new genres to add, add them,
+    and connect genres with game in bridging table"""
     conn = sqlite3.connect("steam.db")
-    cursor = cur_setup()
+    cursor = conn.cursor()
     genres = game[9].split(";")
     for genre in genres:
-        cursor.execute("SELECT name FROM Genres WHERE name = ?;" (genre))
-        current_genre = cursor.fetchone()
-        if not current_genre:
-            cursor.execute("INSERT INTO Genre (name) VALUES (?);", (genre))
+        try:
+            cursor.execute("SELECT name FROM Genre WHERE name = ?;" (genre,))
+        except:  # If not, add the genre in
+            cursor.execute("INSERT INTO Genre (name) VALUES (?);", (genre,))
             conn.commit()
+        # Add the genre and game ids into bridging table
+        cursor.execute("SELECT id FROM Genre WHERE name = ?;", (genre,))
+        genre_for_bridging = str(cursor.fetchone())[1:]
+        genre_for_bridging = genre_for_bridging[:-1]
+        genre_for_bridging = genre_for_bridging[:-1]
+        cursor.execute("INSERT INTO GameGenre VALUES (?, ?);", (game[0], genre_for_bridging))
+        conn.commit()
 
 
 with open("steam.csv", "r", encoding="utf8") as csvfile:
