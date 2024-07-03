63ef8ba097976e569135cfb70ef5faf06df54e9a
Alex Cotiga <20006@burnside.school.nz>
2024-07-03 14:15:21 +1200

    highest rated title and page switching buttons

--- app.py
+++ app.py
@@ -261,13 +261,13 @@ def highest_rated(page):
             next_page = "hide"
         elif page == 1:
             previous = "hide"
-            next_page = "visible"
+            next_page = "next-page"
         elif page == (math.ceil(int(rows[0][0])/LIMIT)):
-            previous = "visible"
+            previous = "previous-page"
             next_page = "hide"
         else:
-            previous = "visible"
-            next_page = "visible"
+            previous = "previous-page"
+            next_page = "next-page"
         results = query_db("SELECT id, name \
                             FROM Game \
                             WHERE negreviews > 0 AND posreviews > 0 \
