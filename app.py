04eb79af9589a98aa470fa1f4a6795cee1860249
Alex Cotiga <20006@burnside.school.nz>
2024-07-04 15:06:55 +1200

    wndows mac and linux titles and page switch buttons

--- app.py
+++ app.py
@@ -405,13 +405,13 @@ def windows_browsing(page):
             next_page = "hide"
         elif page == 1:
             previous = "hide"
-            next_page = "visible"
+            next_page = "next-page"
         elif page == (math.ceil(int(rows[0][0])/LIMIT)):
-            previous = "visible"
+            previous = "previous-page"
             next_page = "hide"
         else:
-            previous = "visible"
-            next_page = "visible"
+            previous = "previous-page"
+            next_page = "next-page"
         results = query_db("SELECT id, name \
                             FROM Game \
                             WHERE windowscompat = 1 \
@@ -438,13 +438,13 @@ def mac_browsing(page):
             next_page = "hide"
         elif page == 1:
             previous = "hide"
-            next_page = "visible"
+            next_page = "next-page"
         elif page == (math.ceil(int(rows[0][0])/LIMIT)):
-            previous = "visible"
+            previous = "previous-page"
             next_page = "hide"
         else:
-            previous = "visible"
-            next_page = "visible"
+            previous = "previous-page"
+            next_page = "next-page"
         results = query_db("SELECT id, name \
                             FROM Game \
                             WHERE maccompat = 1 \
@@ -471,13 +471,13 @@ def linux_browsing(page):
             next_page = "hide"
         elif page == 1:
             previous = "hide"
-            next_page = "visible"
+            next_page = "next-page"
         elif page == (math.ceil(int(rows[0][0])/LIMIT)):
-            previous = "visible"
+            previous = "previous-page"
             next_page = "hide"
         else:
-            previous = "visible"
-            next_page = "visible"
+            previous = "previous-page"
+            next_page = "next-page"
         results = query_db("SELECT id, name \
                             FROM Game \
                             WHERE linuxcompat = 1 \
